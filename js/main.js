6a850715f9819c5f33959c0eb27b53ea7a6b2dc2
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-03-11 16:19:04 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const btnCriar = document.querySelector("#btnCriar");
+let adicionarLembreteNaLista = document.querySelector("#listar_lembrete");
 
 btnCriar.addEventListener("click", () => {
     const nome = document.querySelector("#nome").value;
@@ -8,21 +9,67 @@ btnCriar.addEventListener("click", () => {
     console.log(data);
     console.log(verificaData(data));
 
-    function verificaData(data){
-        let dataInserida = new Date(data);
-        let diaDeHoje = new Date();
-        console.log(diaDeHoje);
-
-        if(dataInserida > diaDeHoje){
-            console.log("Data válida.");
-            return true
-        }else{
-            console.log("Data inválida.");
-            return false
-        }
-    }
-
-    function inserirLembreteNaLista(){
-
+    if(verificaData(data) == true){
+        inserirLembreteNaLista(nome, data);
     }
 })
+
+function verificaData(data){
+    let dataInserida = new Date(data);
+    let diaDeHoje = new Date();
+    console.log(diaDeHoje);
+
+    if(dataInserida > diaDeHoje){
+        console.log("Data válida.");
+        return true
+    }else{
+        console.log("Data inválida.");
+        return false
+    }
+}
+
+function ordenarLembretes() {
+    console.log("ordenarLembretes");
+    const lembretes = Array.from(adicionarLembreteNaLista.children);
+  
+    lembretes.sort((a, b) => {
+      const dataAElement = a.querySelector('p:first-child');
+      const dataA = dataAElement ? new Date(dataAElement.textContent) : null;
+      const dataBElement = b.querySelector('p:first-child');
+      const dataB = dataBElement ? new Date(dataBElement.textContent) : null;
+      return dataA - dataB;
+    });
+  
+    console.log("lembretes", lembretes);
+  
+    lembretes.forEach((lembrete) => {
+      adicionarLembreteNaLista.appendChild(lembrete);
+    });
+  
+    console.log("ordenarLembretes done");
+}    
+
+let contadorLembretes = 0;
+
+function inserirLembreteNaLista(nome, data) {
+    contadorLembretes++;
+    adicionarLembreteNaLista.innerHTML += `
+      <div id="lembrete-${contadorLembretes}">
+        <p>${data}</p>
+        <p>${nome} <button class="apagar_lembrete" data-lembrete-id="${contadorLembretes}">X</button></p>
+        <p>______________________________</p>
+      </div> `
+  
+    const btnApagarLembrete = document.querySelectorAll(".apagar_lembrete");
+  
+    btnApagarLembrete.forEach((botao) => {
+  
+      botao.addEventListener("click", (evento) => {
+        const lembreteId = evento.target.dataset.lembreteId;
+        const lembrete = document.querySelector(`#lembrete-${lembreteId}`);
+        lembrete.remove();
+      });
+    });
+  
+    ordenarLembretes();
+}  
